7e1018f65948a8f19b9ba085cb1b688e8613937a
Navkaran7007 <nsidhu10@rrc.ca>
2025-12-04 11:20:13 -0600

    Add changes to file

--- lambda/get_all_inventory_items/lambda_function.py
+++ lambda/get_all_inventory_items/lambda_function.py
@@ -5,6 +5,7 @@ def lambda_handler(event, context):
     # Initialize a DynamoDB client
     dynamo_client = boto3.client('dynamodb')
 
+
     # Name of the DynamoDB table
     table_name = 'Inventory'
 
